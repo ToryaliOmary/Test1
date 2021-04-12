cffefe2001238a6bc60097f8aa7d6368ce0104dc
Testuser <toryali.omary@stude>
2021-04-12 16:02:38 +0200

    erster Commit zum test

---
+++ Rechnen/src/rechnen.java
@@ -0,0 +1,12 @@
+public class rechnen {
+
+    static int rechnen(int a, int b){
+        int c = a + b;
+        return c;
+    }
+
+    public static void main(String[] args) {
+       int ergebnis = rechnen(2, 3);
+       System.out.println(ergebnis);
+    }
+}
